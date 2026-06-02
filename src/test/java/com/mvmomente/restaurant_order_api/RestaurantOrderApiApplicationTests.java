58a13b14aea3b5e6ae36d800cd6afb171354eb3e
Marcos Momenté <122253692+mvmomente@users.noreply.github.com>
2026-06-01 21:07:47 -0300

    Initial commit

---
+++ src/test/java/com/mvmomente/restaurant_order_api/RestaurantOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvmomente.restaurant_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
